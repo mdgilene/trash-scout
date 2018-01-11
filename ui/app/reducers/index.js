f2a47f9569cc5efbe8d0e03cc1bbe48e7bd53ab3
Matt Gilene <mdgilene@gmail.com>
2018-01-11 10:30:21 -0500

    added flights state to global state

--- ui/app/reducers/index.js
+++ ui/app/reducers/index.js
@@ -2,9 +2,11 @@
 import { combineReducers } from 'redux';
 import { routerReducer as router } from 'react-router-redux';
 
+import flights from './flights';
 import markers from './markers';
 
 const rootReducer = combineReducers({
+  flights,
   markers,
   router
 });
