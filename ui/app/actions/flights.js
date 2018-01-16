2a2e203b2387dc3658b6df228c237527a494060a
Matt Gilene <mdgilene@gmail.com>
2018-01-16 11:55:28 -0500

    added type and updated actions to match the new request format

--- ui/app/actions/flights.js
+++ ui/app/actions/flights.js
@@ -1,7 +1,13 @@
+// @flow
 import axios from 'axios';
 
 export const LOAD_FLIGHTS = 'LOAD_FLIGHTS';
 export const NEW_FLIGHT = 'NEW_FLIGHT';
+
+export type FlightParams = {
+  name: string,
+  imageDensity: number
+};
 
 function loadFlightsAction(flights) {
   return {
@@ -27,9 +33,9 @@ export function loadFlights() {
   };
 }
 
-export function newFlight(flightName) {
+export function newFlight(flightParams: FlightParams) {
   return (dispatch) => {
-    axios.post('http://localhost:3000/flights', { name: flightName })
+    axios.post('http://localhost:3000/flights', { ...flightParams })
       .then(() => {
         dispatch(newFlightAction());
         return dispatch(loadFlights());
