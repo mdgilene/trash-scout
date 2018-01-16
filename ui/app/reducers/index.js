64bec57e567d4e48fdfa40dec1bd448c32415b6e
Matt Gilene <mdgilene@gmail.com>
2018-01-16 11:57:39 -0500

    added app state to global state

--- ui/app/reducers/index.js
+++ ui/app/reducers/index.js
@@ -2,13 +2,15 @@
 import { combineReducers } from 'redux';
 import { routerReducer as router } from 'react-router-redux';
 
+import app from './app';
 import flights from './flights';
 import markers from './markers';
 
 const rootReducer = combineReducers({
+  router,
+  app,
   flights,
   markers,
-  router
 });
 
 export default rootReducer;
